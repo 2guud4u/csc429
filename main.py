058223da0a759b8a094f0c2c62b3f13b0d4acaac
Jiahua Jiang <jiahua212@gmail.com>
2024-04-16 10:33:12 -0700

    ping test

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import check_functionality
+import requests
 
 def main():
+    target = "http://test.csc429.io"
     if(check_functionality.check()):
         #do nothing
         print("All functionality is working fine")
     else:
         #send report
         print("Some functionality is not working")
+
+
+def ping_test(target):
+    try:
+        requests.get(target)
+        return True
+    except:
+        # insert code that runs when site is down here
+        return False
+
+    
