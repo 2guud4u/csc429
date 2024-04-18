680b5e83eb4d710d7834d270f832a554ebbcb614
Jiahua Jiang <jiahua212@gmail.com>
2024-04-18 09:02:14 -0700

    removed while loop for ping

--- main.py
+++ main.py
@@ -17,24 +17,24 @@ def main():
 
 
 def ping_test(target):
-    while True:
-        try:
-            random_number = random.randint(0, 999)
-            payload = {'firstName': 'test', 
-                    'lastName': 'test',
-                    'phoneNumber': random_number, 
-                    'item': 'Wand'}
-            header = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-            r=requests.post(target, headers=header, data=payload)
-            t = time.localtime()
-            current_time = time.strftime("%H:%M:%S", t)
-            print("{time}: {id}".format(time=current_time, id=r.text[-12:]))
-            # check r.text is valid id
-        except:
-            return False
+    try:
+        random_number = random.randint(0, 999)
+        payload = {'firstName': 'test', 
+                'lastName': 'test',
+                'phoneNumber': random_number, 
+                'item': 'Wand'}
+        header = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-        time.sleep(30)
+        r=requests.post(target, headers=header, data=payload)
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        print("{time}: {id}".format(time=current_time, id=r.text[-12:]))
+        # check r.text is valid id
+    except:
+        return False
+
+        
 
 if __name__ == "__main__":
     main()
